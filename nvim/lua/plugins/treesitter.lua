b77c3e9e7f2b63c7eb63607d205ff7fca787810c
David Sapiro <david.sapiro@gmail.com>
2021-06-20 20:36:11 +0100

    moves treesitter config to a file

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+require "nvim-treesitter.configs".setup {
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
+        disable = {"scss"}
+    },
+    rainbow = {
+        enable = false,
+        extended_mode = true -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    },
+    autotag = {
+        enable = true
+    }
+}
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
