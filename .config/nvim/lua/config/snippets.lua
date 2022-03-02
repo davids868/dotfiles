5546ae8a97f86cedbe7bedf6c5eaa33ad1aa3c74
David Sapiro <david.sapiro@gmail.com>
2022-03-02 17:48:31 +0000

    nvim: better snippets mappings

--- .config/nvim/lua/config/snippets.lua
+++ .config/nvim/lua/config/snippets.lua
@@ -1,14 +1,3 @@
--- -- local util = require("utils.mappers")
--- local luasnip = require("luasnip")
-
--- luasnip.config.set_config(
---     {
---         history = true,
---         -- Update more often, :h events for more info.
---         updateevents = "TextChanged,TextChangedI"
---     }
--- )
-
 local function prequire(...)
     local status, lib = pcall(require, ...)
     if (status) then
@@ -17,49 +6,49 @@ local function prequire(...)
     return nil
 end
 
-local luasnip = prequire("luasnip")
-local cmp = prequire("cmp")
+local ls = prequire("luasnip")
 require("luasnip/loaders/from_vscode").load()
 
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
+ls.config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    enable_autosnippets = true
+}
 
-local check_back_space = function()
-    local col = vim.fn.col(".") - 1
-    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        return true
-    else
-        return false
-    end
-end
+-- <c-k> is my expansion key
+-- this will expand the current item or jump to the next item within the snippet.
+vim.keymap.set(
+    {"i", "s"},
+    "<c-k>",
+    function()
+        if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+        end
+    end,
+    {silent = true}
+)
 
-_G.tab_complete = function()
-    if cmp and cmp.visible() then
-        cmp.select_next_item()
-    elseif luasnip and luasnip.expand_or_jumpable() then
-        return t("<Plug>luasnip-expand-or-jump")
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        cmp.complete()
-    end
-    return ""
-end
-_G.s_tab_complete = function()
-    if cmp and cmp.visible() then
-        cmp.select_prev_item()
-    elseif luasnip and luasnip.jumpable(-1) then
-        return t("<Plug>luasnip-jump-prev")
-    else
-        return t "<S-Tab>"
-    end
-    return ""
-end
+-- <c-j> is my jump backwards key.
+-- this always moves to the previous item within the snippet
+vim.keymap.set(
+    {"i", "s"},
+    "<c-j>",
+    function()
+        if ls.jumpable(-1) then
+            ls.jump(-1)
+        end
+    end,
+    {silent = true}
+)
 
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
-vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
+-- <c-l> is selecting within a list of options.
+-- This is useful for choice nodes (introduced in the forthcoming episode 2)
+vim.keymap.set(
+    "i",
+    "<c-l>",
+    function()
+        if ls.choice_active() then
+            ls.change_choice(1)
+        end
+    end
+)
