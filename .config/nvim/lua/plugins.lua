75ba2661bcb6e8329d9ce7de99dbe04782c242dc
David Sapiro <david.sapiro@gmail.com>
2022-04-11 13:16:38 +0100

    nvim(fix): git plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -627,30 +627,32 @@ local function plugins(use)
 
     -- Git
     use {
-        "tpope/vim-fugitive",
-        config = function()
-            local nnoremap = require("utils.mappers").nnoremap
-            local vnoremap = require("utils.mappers").vnoremap
-            local g = vim.g
+        {
+            "tpope/vim-fugitive",
+            config = function()
+                local nnoremap = require("utils.mappers").nnoremap
+                local vnoremap = require("utils.mappers").vnoremap
+                local g = vim.g
 
-            g.fugitive_gitlab_domains = {"https://gitlab.eu-west-1.mgmt.onfido.xyz/"}
+                g.fugitive_gitlab_domains = {"https://gitlab.eu-west-1.mgmt.onfido.xyz/"}
 
-            nnoremap {"<leader>ge", ":GBrowse<CR>"}
-            nnoremap {"<leader>gy", ":GBrowse!<CR>"}
-            vnoremap {"<leader>ge", ":'<,'>GBrowse<CR>"}
-            vnoremap {"<leader>gy", ":'<,'>GBrowse!<CR>"}
-            nnoremap {"<leader>df", ":Gvdiffsplit origin/master<CR>"}
-            nnoremap {"<leader>dv", ":DiffviewOpen<CR>"}
-            nnoremap {"<leader>dm", ":DiffviewOpen origin/master<CR>"}
-            nnoremap {"<leader>fh", ":DiffviewFileHistory<CR>"}
-            nnoremap {"<leader>bc", ":DiffviewFileHistory<CR>"}
-            nnoremap {"<leader>dl", ":diffget //3<CR>"}
-            nnoremap {"<leader>dh", ":diffget //2<CR>"}
-            nnoremap {"<leader>gs", ":tab G<CR>"}
-            nnoremap {"<leader>gb", ":Git blame<CR>"}
-            nnoremap {"<leader>gc", ":GV<CR>"}
-            nnoremap {"<leader>dv", ":DiffviewOpen<CR>"}
-        end,
+                nnoremap {"<leader>ge", ":GBrowse<CR>"}
+                nnoremap {"<leader>gy", ":GBrowse!<CR>"}
+                vnoremap {"<leader>ge", ":'<,'>GBrowse<CR>"}
+                vnoremap {"<leader>gy", ":'<,'>GBrowse!<CR>"}
+                nnoremap {"<leader>df", ":Gvdiffsplit origin/master<CR>"}
+                nnoremap {"<leader>dv", ":DiffviewOpen<CR>"}
+                nnoremap {"<leader>dm", ":DiffviewOpen origin/master<CR>"}
+                nnoremap {"<leader>fh", ":DiffviewFileHistory<CR>"}
+                nnoremap {"<leader>bc", ":DiffviewFileHistory<CR>"}
+                nnoremap {"<leader>dl", ":diffget //3<CR>"}
+                nnoremap {"<leader>dh", ":diffget //2<CR>"}
+                nnoremap {"<leader>gs", ":tab G<CR>"}
+                nnoremap {"<leader>gb", ":Git blame<CR>"}
+                nnoremap {"<leader>gc", ":GV<CR>"}
+                nnoremap {"<leader>dv", ":DiffviewOpen<CR>"}
+            end
+        },
         {
             "tpope/vim-rhubarb",
             "shumphrey/fugitive-gitlab.vim",
