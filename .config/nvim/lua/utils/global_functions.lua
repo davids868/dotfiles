c67cad27f65ede844a88133f07534ba3b20a85e7
David Sapiro <david.sapiro@gmail.com>
2022-04-14 16:20:59 +0100

    nvim: remove global dump method as it is now provided by vim.pretty_print

--- .config/nvim/lua/utils/global_functions.lua
+++ .config/nvim/lua/utils/global_functions.lua
@@ -1,8 +1,3 @@
-function _G.dump(...)
-    local objects = vim.tbl_map(vim.inspect, {...})
-    print(unpack(objects))
-end
-
 function _G.new_tab()
     local ft = vim.bo.filetype
     vim.api.nvim_command(":tabnew")
