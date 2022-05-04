523156c34219df3b8b8d4e0d4dec64ac1ccd8a45
David Sapiro <david.sapiro@gmail.com>
2022-05-04 09:46:30 +0100

    nvim: adds benchmarking global function

--- .config/nvim/lua/utils/global_functions.lua
+++ .config/nvim/lua/utils/global_functions.lua
@@ -12,3 +12,35 @@ R = function(name)
   RELOAD(name)
   return require(name)
 end
+
+local units = {
+  ["seconds"] = 1,
+  ["milliseconds"] = 1000,
+  ["microseconds"] = 1000000,
+  ["nanoseconds"] = 1000000000,
+}
+
+-- USAGE: benchmark("milliseconds", 2, 500, test, 10000)
+function benchmark(unit, decPlaces, n, f, ...)
+  local elapsed = 0
+  local multiplier = units[unit]
+  for i = 1, n do
+    local now = os.clock()
+    f(...)
+    elapsed = elapsed + (os.clock() - now)
+  end
+  print(
+    string.format(
+      "Benchmark results: %d function calls | %."
+        .. decPlaces
+        .. "f %s elapsed | %."
+        .. decPlaces
+        .. "f %s avg execution time.",
+      n,
+      elapsed * multiplier,
+      unit,
+      (elapsed / n) * multiplier,
+      unit
+    )
+  )
+end
