8cb9f69f54ad2b397abe6267b924e8b90a734f3e
David Sapiro <david.sapiro@gmail.com>
2022-05-18 17:57:22 +0100

    nvim: adds treesitter incremental selection

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -73,4 +73,13 @@ require("nvim-treesitter.configs").setup {
     ["ac"] = "@class.outer",
     ["ic"] = "@class.inner",
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
