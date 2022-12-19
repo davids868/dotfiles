0134835389a3e4027aaf991c409ef628dae58e4d
David Sapiro <david.sapiro@gmail.com>
2022-12-19 10:44:09 +0000

    nvim: move symbol outline to left side

--- .config/nvim/lua/config/symbol-outline.lua
+++ .config/nvim/lua/config/symbol-outline.lua
@@ -2,6 +2,9 @@ local opts = {
   highlight_hovered_item = true,
   show_guides = true,
   auto_preview = false,
+  position = "left",
+  relative_width = false,
+  width = 50,
 }
 
 require("symbols-outline").setup(opts)
